ea82e01a5c9edb39d411ec9762c22b45b926a2ba
haofano <wd1786@163.com>
2018-06-07 20:26:00 +0800

    day607

---
+++ src/main/java/com/dong/tmall/service/UserService.java
@@ -0,0 +1,13 @@
+package com.dong.tmall.service;
+
+import com.dong.tmall.pojo.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User c);
+    void delete(int id);
+    void update(User c);
+    User get(int id);
+    List list();
+}
